e5f0d86d422ad7093456433b49760133f52a5204
PsyFabriQ <psyfabriqdev@gmail.com>
2018-01-24 16:08:26 +0300

    update

---
+++ server/src/main/java/pfq/storage/server/PostConstructExampleBean.java
@@ -0,0 +1,36 @@
+package pfq.storage.server;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import pfq.storage.server.dao.UserDAO;
+import pfq.storage.server.model.Role;
+import pfq.storage.server.model.User;
+
+@Component
+public class PostConstructExampleBean {
+ 
+    @Autowired
+    private UserDAO userDao;
+ 
+    @PostConstruct
+    public void init() {
+    	if(!userDao.checkHasUser("admin")) {
+			 System.out.println("Not Check Admin User");
+			 Role r = new Role();
+			 r.setName(Role.Enum.ADMIN.toString());
+			 User au = User.newBuilder()
+					  .setLogin("admin")
+					  .setName("Admin")
+					  .setEmail("admin@system.com")
+					  .setPassword("admin")
+					  .setUserRoles(new Role().setName(Role.Enum.USER.toString()))
+					  .setUserRoles(new Role().setName(Role.Enum.ADMIN.toString()))
+					  .build();	
+			 System.out.println(au.toString());
+			 userDao.addUser(au);
+		}
+    }
+}
